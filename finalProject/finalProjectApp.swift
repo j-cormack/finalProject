d95a9729440822313ed9dda6b371828edc5e9aad
Jess <jessella29@outlook.com>
2023-07-14 19:44:30 +0100

    Initial Commit

---
+++ finalProject/finalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectApp.swift
+//  finalProject
+//
+//  Created by Jess Cormack on 14/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
